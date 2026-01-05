76fed2e8b578002283764a1fd32e712fdbb34c78
Joe Loveless <jloveless03@pm.me>
2026-01-05 14:51:10 -0600

    Create Microblog.Common.psm1

---
+++ scripts/microblog/common/Microblog.Common.psm1
@@ -0,0 +1,17 @@
+$publicPath  = Join-Path $PSScriptRoot "common"
+$privatePath = Join-Path $PSScriptRoot "common/private"
+
+# Load all shared public + private functions
+$Public  = @( Get-ChildItem -Path $publicPath  -Filter *.ps1 -Recurse -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $privatePath -Filter *.ps1 -Recurse -ErrorAction SilentlyContinue )
+
+foreach ($import in @($Public + $Private)) {
+    try {
+        . $import.FullName
+    }
+    catch {
+        Write-Error "Failed to import shared function $($import.FullName): $_"
+    }
+}
+
+Export-ModuleMember -Function $Public.BaseName
