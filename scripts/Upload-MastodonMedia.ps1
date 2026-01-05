429fe53ed060970cb18e1b471864b3cf44c1f9dc
Joe Loveless <jloveless03@pm.me>
2026-01-05 11:49:05 -0600

    Simplify Invoke-RestMethod call in script

---
+++ scripts/Upload-MastodonMedia.ps1
@@ -0,0 +1,21 @@
+function Upload-MastodonMedia {
+    param (
+        [string]$Instance,
+        [string]$Token,
+        [string]$Path,
+        [string]$Alt
+    )
+
+    if (-not (Test-Path $Path)) {
+        Write-Warning "Image file not found: $Path"
+        return $null
+    }
+
+    $headers = @{ Authorization = "Bearer $Token" }
+    $form = @{ file = Get-Item $Path }
+
+    if ($Alt) { $form.description = $Alt }
+
+    $res = Invoke-RestMethod -Uri "https://$Instance/api/v1/media" -Method POST -Headers $headers -Form $form
+    return $res.id
+}
