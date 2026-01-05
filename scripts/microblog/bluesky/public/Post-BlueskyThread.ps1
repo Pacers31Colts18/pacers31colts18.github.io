753dd299dd65b17045977f3de068760b6f66f038
Joe Loveless <jloveless03@pm.me>
2026-01-05 14:56:16 -0600

    Create Post-BlueskyThread.ps1

---
+++ scripts/microblog/bluesky/public/Post-BlueskyThread.ps1
@@ -0,0 +1,51 @@
+function Post-BlueskyThread {
+    param(
+        [Parameter(Mandatory)]
+        $Session,
+
+        [Parameter(Mandatory)]
+        [string[]] $Posts,
+
+        [Parameter()]
+        $Media
+    )
+
+    $previous = $null
+    $results = @()
+
+    foreach ($post in $Posts) {
+
+        $embed = $null
+
+        if ($Media) {
+            $embed = @{
+                images = @(
+                    @{
+                        image = $Media
+                        alt   = "Image"
+                    }
+                )
+                "$type" = "app.bsky.embed.images"
+            }
+        }
+
+        if ($previous) {
+            $embed = $embed ?? @{}
+            $record = @{
+                text      = $post
+                createdAt = (Get-Date).ToString("o")
+                reply     = @{
+                    root = $previous
+                    parent = $previous
+                }
+            }
+        }
+
+        $response = New-BlueskyPost -Session $Session -Text $post -Embed $embed
+        $previous = $response.uri
+
+        $results += $response
+    }
+
+    return $results
+}
