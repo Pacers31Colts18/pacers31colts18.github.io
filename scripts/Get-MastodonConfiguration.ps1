f5766bb2e56d5f0c42199330bb939eb9ff7337ab
Joe Loveless <jloveless03@pm.me>
2026-01-05 11:47:06 -0600

    Add Get-MicroblogConfig function for Mastodon

---
+++ scripts/Get-MastodonConfiguration.ps1
@@ -0,0 +1,20 @@
+function Get-MicroblogConfig {
+    $config = [ordered]@{
+        Instance = $env:MASTODON_INSTANCE
+        Token    = $env:MASTODON_TOKEN
+        File     = "microblog/microblog.md"
+        MaxChars = 500
+        MaxMedia = 4
+        Pat      = $env:GH_PAT
+    }
+
+    if (-not $config.Instance -or -not $config.Token) {
+        throw "Missing Mastodon configuration"
+    }
+
+    if (-not (Test-Path $config.File)) {
+        throw "Microblog file not found: $($config.File)"
+    }
+
+    return $config
+}
